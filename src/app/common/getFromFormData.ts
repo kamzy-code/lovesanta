bb9f159af2a6e562bae62dc6a92b00f9656382a6
SPEED OF LIGHT <mac@Mac-Macbook-Pro.local>
2026-01-08 23:42:06 -0800

    formdata string conversion fix

--- src/app/common/getFromFormData.ts
+++ src/app/common/getFromFormData.ts
@@ -1,4 +1,4 @@
 export const get = (key: string, formData: FormData) => {
   const v = formData.get(key);
-  return v === null ? "" : String(v).trim();
+  return typeof v === "string" ? v.trim() : "";
 };
